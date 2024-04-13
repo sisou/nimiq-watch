2ed09d6832b41f6869e64e7be873b411124abe72
Sören <hello@soerenschwert.de>
2024-04-14 00:43:38 +0200

    Add #faucet to directNavigationTargets

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ var $infobox        = document.getElementById('infobox'),
     $status         = document.getElementById('status'),
     $height         = document.getElementById('height');
 
-var directNavigationTargets = ['#about', '#charts', '#labels'];
+var directNavigationTargets = ['#about', '#charts', '#labels', '#faucet'];
 
 var default_colors = ['#3366CC','#DC3912','#FF9900','#109618','#990099','#3B3EAC','#0099C6','#DD4477','#66AA00','#B82E2E','#316395','#994499','#22AA99','#AAAA11','#6633CC','#E67300','#8B0707','#329262','#5574A6','#3B3EAC'];
 default_colors = default_colors.concat(default_colors, default_colors);
