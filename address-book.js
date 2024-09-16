48d4408e2a1fc88d5617e21155c7662f50224e8c
nim.re <nim.re-github@mw.gd>
2024-09-16 14:35:08 +0200

    Update address-book.js
    Added nim.re's address to validator registrations list.

--- address-book.js
+++ address-book.js
@@ -423,6 +423,7 @@ AddressBook.BOOK = {
     // '': 'AceStaking',
     'NQ37 6EL5 BP9K XL1A 3ED0 L3EC NPR5 C9D3 BRKG': 'Helvetia Staking',
     'NQ53 M1NT S3JD TAGM CBTK 01PX YD3U B1DE GYHB': 'Mint Pool',
+    'NQ44 V95C ABVY RARR SBMC V8VE M6UH EJP0 RXYQ': 'nim.re',
 
     // Testnet
     'NQ31 QEPR ED7V 00KC P7UC P1PR DKJC VNU7 E461': 'pool.nimiq-testnet.com',
